10c3cce0b188f578b53ba2dbbdb310deeb0ebc06
emanuelgois <emanuel.gois@gmail.com>
2020-01-07 22:03:46 -0300

    Projeto inicial

---
+++ src/main/java/com/megastatics/MegastaticsApplication.java
@@ -0,0 +1,13 @@
+package com.megastatics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegastaticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegastaticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/megastatics/MegastaticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.megastatics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MegastaticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
