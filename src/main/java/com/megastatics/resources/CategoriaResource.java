77c1e8b539e7f488663da0ed706137a4240db78e
emanuelgois <emanuel.gois@gmail.com>
2020-01-22 20:38:54 -0300

    ajustes

--- src/main/java/com/megastatics/resources/CategoriaResource.java
+++ src/main/java/com/megastatics/resources/CategoriaResource.java
@@ -1,11 +1,17 @@
 package com.megastatics.resources;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.megastatics.domain.Categoria;
 import com.megastatics.services.CategoriaService;
@@ -21,6 +27,15 @@ public class CategoriaResource {
 	public ResponseEntity<?> find(@PathVariable Integer id) {
 		Categoria obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody Categoria obj) {
+		//Categoria obj = service.fromDTO(objDto);
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+			.path("/{id}").buildAndExpand(obj.getCodigo()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 	
